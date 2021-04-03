5606b1f82c91d5f1e6c9c905ed88f6c792b68924
Oscar Armando Cruz Mendez <ocruz@caracolknits.com>
2021-04-03 00:30:10 -0600

    agregando pantalla de login

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import RoutesAuth from "./src/Navigation/RoutesAuth";
+import RoutesNotAuth from "./src/Navigation/RoutesNotAuth";
 
 export default function App() {
-  return <RoutesAuth />;
+  return <RoutesNotAuth />;
 }
 
 const styles = StyleSheet.create({
--- src/Views/Accounts/Login.js
+++ src/Views/Accounts/Login.js
@@ -1,10 +1,39 @@
-import React from "react";
-import { View, Text } from "native-base";
+import React, { useRef } from "react";
+import { View, Text, StyleSheet, Image, StatusBar } from "react-native";
+import LoginForm from "../../Components/LoginForm";
+import Toast from "react-native-easy-toast";
 
 export default function Login() {
+  const toastRef = useRef();
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#128C73" />
+      <Image
+        source={require("../../../assets/logo.png")}
+        style={styles.imglogo}
+      />
+      <Text style={styles.textbaner}>¡Bienvenido!</Text>
+      <LoginForm toastRef={toastRef} />
+      <Toast ref={toastRef} position="bottom" opacity={0.9} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#128C73",
+  },
+  imglogo: {
+    width: 150,
+    height: 110,
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  textbaner: {
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "#fff",
+    alignSelf: "center",
+  },
+});
